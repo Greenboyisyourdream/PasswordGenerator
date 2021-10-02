794cf45ec0bfeed638e3befd82c301d78d843f3c
Greenboyisyourdream <pankazimierzwlesie@protonmail.com>
2021-10-02 15:42:17 +0200

    v0.31

--- app.py
+++ app.py
@@ -4,30 +4,30 @@ from string import ascii_lowercase, ascii_uppercase, digits
 from gui import window, screen
 
 while True:
-    symbols = set()
+    symbols = set(ascii_uppercase)
     button, value = window.read()
     lenght = value["LEN"]
+    if value["H"]:
+        symbols.union(ascii_uppercase)
+    else:
+        symbols.intersection(ascii_uppercase)
+    if value["L"]:
+        symbols.union(ascii_lowercase)
+    else:
+        symbols.intersection(ascii_lowercase)
+    if value["D"]:
+        symbols.union(digits)
+    else:
+        symbols.intersection(digits)
+    if value["S"]:
+        symbols.union(
+            ["!", "@", "#", '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '|', "_", "+", "=", "-", "."])
+    else:
+        symbols.intersection(
+            ["!", "@", "#", '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '|', "_", "+", "=", "-", "."])
     if button in [None, "Quit"]:
         break
     elif button == "Generate!":
         screen.format()
-        screen = str(*[random.choice(list(symbols)) for _ in range(lenght)])
+        screen = "".join(random.choice(list(symbols)) for _ in range(int(lenght)))
         window.FindElement("output").Update(screen)
-    elif value["H"]:
-        symbols.union(ascii_uppercase)
-    elif value["H"] is False:
-        symbols.intersection(ascii_uppercase)
-    elif value["L"]:
-        symbols.union(ascii_lowercase)
-    elif value["L"] is False:
-        symbols.intersection(ascii_lowercase)
-    elif value["D"]:
-        symbols.union(digits)
-    elif value["D"] is False:
-        symbols.intersection(digits)
-    elif value["S"]:
-        symbols.union(
-            ["!", "@", "#", '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '|', "_", "+", "=", "-", "."])
-    elif value["S"] is False:
-        symbols.intersection(
-            ["!", "@", "#", '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '|', "_", "+", "=", "-", "."])
